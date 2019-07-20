3a3d99a6e889cbeadfb367de99b6e88609eef7f4
Makram Jandar <makramjandar@gmail.com>
2019-07-21 01:10:11 +0200

    Create genCert.sh

---
+++ dockerfiles/fastai/fastai/genCert.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+openssl \
+  req \
+  -newkey rsa:2048 -nodes \
+  -keyout pkey.pem \
+  -x509 -days 36500 -out cert.pem \
+  -subj "/C=FR/ST=PARIS/L=Earth/O=Jupyter/OU=IT/CN=www.makramjandar.com/emailAddress=makramjandar@gmail.com"
