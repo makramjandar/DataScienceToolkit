2f85346b80e74f96f6fbd24b12b9d75d2510b359
AIe !! <makramjandar@gmail.com>
2020-08-23 12:10:32 +0200

    ✓

--- dockerfiles/pytorch/fastai/genCert.sh
+++ dockerfiles/pytorch/fastai/genCert.sh
@@ -5,4 +5,4 @@ openssl \
   -newkey rsa:2048 -nodes \
   -keyout pkey.pem \
   -x509 -days 36500 -out cert.pem \
-  -subj "/C=FR/ST=PARIS/L=Earth/O=Jupyter/OU=IT/CN=www.makramjandar.com/emailAddress=makramjandar@gmail.com"
+  -subj "/C=FR/ST=PARIS/L=Earth/O=Jupyter/OU=IT/CN=www.makramjandar.com/emailAddress=makramjandar@gmail.com"  
